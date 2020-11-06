99766631ea6619b52657dd9892d00023256f72c2
1210takumi <uw.t.t.t.takumi@icloud.com>
2020-11-06 14:13:34 +0900

    fastcommit

--- db/schema.rb
+++ db/schema.rb
@@ -53,6 +53,20 @@ ActiveRecord::Schema.define(version: 2020_10_31_084317) do
     t.index ["user_id"], name: "index_prototypes_on_user_id"
   end
 
+  create_table "routes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "name", null: false
+    t.text "profile", null: false
+    t.text "occupation", null: false
+    t.text "position", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
